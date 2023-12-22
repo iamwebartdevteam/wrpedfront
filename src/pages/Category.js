aae2e419fad5a077bb54abf1297052dceb767d89
webarttechnology <jordan.digitalwebber1@gmail.com>
2023-12-22 13:48:51 +0530

    song catagory wais data

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -89,12 +89,114 @@ const Category = ({ setIsLogin }) => {
   return (
     <>
       {/* <InnerBanner /> */}
-      <div class="ms_genres_wrapper">
+      <div class="ms_genres_wrapper  mt-0">
         <div class="row justify-content-center">
           {/* <div className="col-md-12">
             <h2 className="text-center songHead">Songs Selection</h2>
           </div> */}
-          <div class="col-md-7">
+          <div class="col-md-7 cataTeb">
+            <ul
+              class="nav nav-pills mb-3 justify-content-center"
+              id="pills-tab"
+              role="tablist"
+            >
+              <li class="nav-item" role="presentation">
+                <button
+                  class="nav-link active"
+                  id="pills-home-tab"
+                  data-bs-toggle="pill"
+                  data-bs-target="#pills-home"
+                  type="button"
+                  role="tab"
+                  aria-controls="pills-home"
+                  aria-selected="true"
+                  onClick={() => categoriy_details("4")}
+                >
+                  All
+                </button>
+              </li>
+              <li class="nav-item" role="presentation">
+                <button
+                  class="nav-link"
+                  id="pills-profile-tab"
+                  data-bs-toggle="pill"
+                  data-bs-target="#pills-profile"
+                  type="button"
+                  role="tab"
+                  aria-controls="pills-profile"
+                  aria-selected="false"
+                >
+                  Genre
+                </button>
+              </li>
+              <li class="nav-item" role="presentation">
+                <button
+                  class="nav-link"
+                  id="pills-contact-tab"
+                  data-bs-toggle="pill"
+                  data-bs-target="#pills-contact"
+                  type="button"
+                  role="tab"
+                  aria-controls="pills-contact"
+                  aria-selected="false"
+                >
+                  Occasion
+                </button>
+              </li>
+              <li class="nav-item" role="presentation">
+                <button
+                  class="nav-link"
+                  id="pills-mood-tab"
+                  data-bs-toggle="pill"
+                  data-bs-target="#pills-mood"
+                  type="button"
+                  role="tab"
+                  aria-controls="pills-mood"
+                  aria-selected="false"
+                >
+                  Mood
+                </button>
+              </li>
+            </ul>
+            <div class="tab-content" id="pills-tabContent">
+              <div
+                class="tab-pane fade show active"
+                id="pills-home"
+                role="tabpanel"
+                aria-labelledby="pills-home-tab"
+                tabindex="0"
+              >
+                ...
+              </div>
+              <div
+                class="tab-pane fade"
+                id="pills-profile"
+                role="tabpanel"
+                aria-labelledby="pills-profile-tab"
+                tabindex="0"
+              >
+                ...
+              </div>
+              <div
+                class="tab-pane fade"
+                id="pills-contact"
+                role="tabpanel"
+                aria-labelledby="pills-contact-tab"
+                tabindex="0"
+              >
+                ...
+              </div>
+              <div
+                class="tab-pane fade"
+                id="pills-mood"
+                role="tabpanel"
+                aria-labelledby="pills-mood-tab"
+                tabindex="0"
+              >
+                ...
+              </div>
+            </div>
+
             <div class="cat action">
               <label onClick={activeButton} class={isActive ? "" : "active"}>
                 <span>All</span>
